88600f5baf142e9203145bbd2f15a2c5b1e27577
Alejandro Gómez <alejandrogomezmartin90@gmail.com>
2022-05-23 18:34:22 +0200

    * Update saludo.sh

--- scripts/saludo.sh
+++ scripts/saludo.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 
-# VARIABLES Y FUNCONES
-DIR_HOME=$(cd `dirname $0` && pwd)
-source "${DIR_HOME}/commonFunctions.sh"
-SCRIPT_NAME=`getStriptNameWithoutExtension $0`
-
-declare -A script_info
-export script_info=(
-	[name]="${SCRIPT_NAME}" 
-	[location]="${DIR_HOME}" 
-	[description]="My large description" 
-	[calling]="./`getStriptName $0` "
-)
-
-showScriptInfo
-
 # sudo apt install figlet cowsay fortune fortunes-es fortunes-es-off
 
 vacas=(`ls /usr/share/cowsay/cows`)
 TVACAS=${#vacas[*]}
 NVACA=$((RANDOM%$TVACAS))
 vaca=${vacas[$NVACA]}
-figlet Alex Mola y Lo sabes
+figlet "Hola mundo"
 fortune | cowsay -f $vaca

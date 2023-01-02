1cd901f7ba1451b91bc7a348c174aeb9182a3d96
alejandrogm90 <alejandrogomezmartin90@gmail.com>
2023-01-02 09:15:33 +0100

    + Add function getValueFromJsonFileStructured

--- scripts/commonFunctions.sh
+++ scripts/commonFunctions.sh
@@ -138,3 +138,8 @@ function reverseList {
         echo ${valores[$indice]}
     done
 }
+
+# RETUR THE VALUE FROM A JSON FILE
+function getValueFromJsonFileStructured() {
+	echo "`cat $1 | grep $2 | cut -d'=' -f2`"
+}
